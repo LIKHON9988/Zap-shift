d9d07f42b521222abba2d13db3d2e66ad085f326
LIKHON9988 <likhondatta343@gmail.com>
2025-11-26 19:45:33 +0600

    server added

--- src/hooks/UseAxios.jsx
+++ src/hooks/UseAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const axiosSequre = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://zap-shift-server-sigma.vercel.app",
 });
 
 const UseAxios = () => {
